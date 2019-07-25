b66aed4d5b00ca736957c2ae1d2d8e3cc4a483de
Victoire <victoirebaron@hotmail.fr>
2019-07-25 00:13:54 -0700

    Updates

---
+++ precache-manifest.fa449d8bb2982b343f393bcb636d4694.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "161b52dea9bb00f64b077b5f84b6cb87",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "7bef57ad7b8c8794b09d",
+    "url": "/clicky-game/static/css/main.da28f9a4.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/clicky-game/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "7bef57ad7b8c8794b09d",
+    "url": "/clicky-game/static/js/main.9b629821.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  },
+  {
+    "revision": "1afbd0a90eaad3b77a98717552d48874",
+    "url": "/clicky-game/static/media/forest.1afbd0a9.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/clicky-game/precache-manifest.211f3c4a8eca55ab3b7dabc38a4e5ae1.js"
+  "/clicky-game/precache-manifest.fa449d8bb2982b343f393bcb636d4694.js"
 );
 
 self.addEventListener('message', (event) => {
